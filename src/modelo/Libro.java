8d7c5d106ede0afe1bd74453cb90aa671e35fbe5
Alejandro Sariñena <jandro.github@gmail.com>
2026-09-26 18:09:51 +0200

    Creado el modelo Libro.java

--- src/modelo/Libro.java
+++ src/modelo/Libro.java
@@ -2,4 +2,66 @@ package modelo;
 
 public class Libro {
 
+	private String id;
+    private String titulo;
+    private String autor;
+    private double precio;
+    private int stock;
+    
+	public Libro(String id, String titulo, String autor, double precio, int stock) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.precio = precio;
+		this.stock = stock;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "ID Libro: " + id + " | Titulo: " + titulo + " | Autor: " + autor + " | Precio: " + precio + " € | Stock: " + stock;
+	}
+    
+    
+	
 }
