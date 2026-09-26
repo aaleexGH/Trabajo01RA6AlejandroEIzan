8c3a11e90a67d8cb23938df6a93f135a2537b8a1
Alejandro Sariñena <jandro.github@gmail.com>
2026-09-26 18:11:53 +0200

    Hecho la interfaz Libro Repository

--- src/repositorios/LibroRepository.java
+++ src/repositorios/LibroRepository.java
@@ -1,5 +1,16 @@
 package repositorios;
 
-public interface LibroRepository {
+import java.util.List;
 
+import modelo.Libro;
+
+public interface LibroRepository {
+    List<Libro> findAll();
+    List<Libro> findByTitulo(String titulo);
+    List<Libro> findByAutor(String autor);
+    List<Libro> findByRangoPrecio(double min, double max);
+    List<Libro> findByStockMinimo(int stockMinimo);
+    void insertar(Libro libro);    
+    void eliminarPorId(String id);
+    void guardarTodos(List<Libro> libros);
 }
